f5f3943c8cfce9a3cd7074045fbd05e12d53ac76
Tatiana-Juin <tatiana.juinp15@gmail.com>
2024-01-24 18:24:12 +0100

    debut du fichier script

---
+++ script.js
@@ -0,0 +1,7 @@
+// ELEMENT SELECTIONNER DU DOM 
+let age = document.getElementById("age");
+let taille = document.getElementById("taille");
+let poids = document.getElementById("poids");
+let btn = document.querySelector(".btn");
+let reponse = document.querySelector(".reponse");
+
